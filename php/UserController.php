d460fe115ec8b3786cc3f0775a152f11eba76bcc
antay1551 <antay1551@gmail.com>
2019-04-04 00:05:34 +0300

    add account front

--- php/UserController.php
+++ php/UserController.php
@@ -7,10 +7,7 @@ require_once 'LoginUser.php';
         $registerUser->connect();
     }
     if(isset($_POST["login"])){
-        //print_r($_POST);
         $loginUser = new LoginUser($_POST);
         $loginUser->connect();
-        //print($_SESSION["id_user"]);
-        //print_r($_POST);        //header('Location: index.html', true, 303 );
         header("Location: http://project.test/home.php");
     }
